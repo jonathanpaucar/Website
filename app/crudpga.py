2cf7fb27bd279fc6632aede4c1eb619365ce4326
jonathanpaucar <jonathanpaucar@gmail.com>
2022-08-16 22:26:33 -0400

    aa

--- app/crudpga.py
+++ app/crudpga.py
@@ -3,5 +3,5 @@ from sqlalchemy.orm import Session
 from . import modelspga
 
 def get_pgastats(db: Session):
-    return db.query(modelspga.pgastats.player, modelspga.pgastats.numofrounds, modelspga.pgastats.avgapproach, modelspga.pgastats.totalapproach, modelspga.pgastats.pergreensinreg,
-    modelspga.pgastats.greenshitinreg, modelspga.pgastats.numholes).all()
+    return db.query(modelspga.pga.player, modelspga.pga.numofrounds, modelspga.pga.avgapproach, modelspga.pga.totalapproach, modelspga.pga.pergreensinreg,
+    modelspga.pga.greenshitinreg, modelspga.pga.numholes).all()
