56a7919b436758e1eef8f4ad11494bd7949f09f5
jonathanpaucar <jonathanpaucar@gmail.com>
2022-08-16 21:03:35 -0400

    aa

--- app/crud.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy.orm import Session
-
-from . import models
-
-def get_salary(db: Session):
-    return db.query(models.salary.player, models.salary.fieldposition, models.salary.team, models.salary.salary).all()
