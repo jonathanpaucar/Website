9998322603e873a72f544d081bc863db0c86f0d1
jonathanpaucar <jonathanpaucar@gmail.com>
2022-08-22 13:16:04 -0400

    aa

--- app/modelspga.py
+++ app/modelspga.py
@@ -3,6 +3,7 @@ from .database import Base
 
 class pga(Base):
     __tablename__ = "pgastats"
+    id = Column(Integer, primary_key=True, index=True)
     player = Column(String, unique=True, index=True)
     numofrounds = Column(Numeric)
     avgapproach = Column(Numeric)
